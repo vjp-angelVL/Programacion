3ed48415319c977c5cc89abba92ba47042c312f3
vjp-angelVL <avegasl01@informatica.iesvalledeljerteplasencia.es>
2025-09-30 16:20:54 +0200

    Revert "Ejercicios"
    This reverts commit 478359b37d44ba9a183d8fddd416f9d9ee4f0ffd.

---
+++ Tema2/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+/**
+ * @author Ángel Vegas López
+ */
+public class Ejercicio4 {
+    
+    public static void main(String[] args) {
+        float nota1=8.7F;
+        float nota2=9.6F;
+        int notamedia=nota1+nota2
+        System.out.println("Programación");
+        System.out.println("La nota del primer examen es" +nota1+ "y la del segundo examen es" +nota2);
+        System.out.println("la nota media es" + nota1 + "+" +nota2);
+            
+    }
+}
